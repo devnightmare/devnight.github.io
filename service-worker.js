7e150e7bd13e075848e2e8be2bb9c46c6db610ec
Ricky <twindestiny1@yahoo.com>
2022-03-11 13:10:45 -0600

    dds

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function (reg) {
+    navigator.serviceWorker.register('devnight.github.io/sw.js').then(function (reg) {
 
         if (reg.installing) {
             console.log('Service worker installing');
